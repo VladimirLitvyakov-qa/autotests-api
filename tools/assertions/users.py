ff080a3465f2032c4f6c278c20ef5e0bdce80134
Vladimir Litvyakov <flapsya@ya.ru>
2025-09-06 11:15:07 +0800

    Добавление Allure шагов в автотесты

--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,7 +1,15 @@
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, UserSchema
+import allure
+
+from clients.users.users_schema import (
+    CreateUserRequestSchema,
+    CreateUserResponseSchema,
+    UserSchema,
+    GetUserResponseSchema
+)
 from tools.assertions.base import assert_equal
 
 
+@allure.step("Check create user response")
 def assert_create_user_response(request: CreateUserRequestSchema, response: CreateUserResponseSchema):
     """
     Проверяет, что ответ на создание пользователя соответствует запросу.
@@ -16,6 +24,7 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
     assert_equal(response.user.middle_name, request.middle_name, "middle_name")
 
 
+@allure.step("Check user")
 def assert_user(actual: UserSchema, expected: UserSchema):
     """
     Проверяет, что фактические данные пользователя соответствуют ожидаемым.
@@ -31,7 +40,11 @@ def assert_user(actual: UserSchema, expected: UserSchema):
     assert_equal(actual.middle_name, expected.middle_name, "middle_name")
 
 
-def assert_get_user_response(get_user_response, create_user_response):
+@allure.step("Check get user response")
+def assert_get_user_response(
+        get_user_response: GetUserResponseSchema,
+        create_user_response: CreateUserResponseSchema
+):
     """
     Проверяет, что ответ на получение пользователя соответствует ответу на его создание.
 
@@ -39,4 +52,4 @@ def assert_get_user_response(get_user_response, create_user_response):
     :param create_user_response: Ответ API при создании пользователя.
     :raises AssertionError: Если данные пользователя не совпадают.
     """
-    assert_user(actual=get_user_response.user, expected=create_user_response.user)
+    assert_user(get_user_response.user, create_user_response.user)
