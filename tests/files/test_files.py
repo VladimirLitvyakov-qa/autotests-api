35428aff480656ebb6f6a0682934291c1cf1a0f2
Vladimir Litvyakov <flapsya@ya.ru>
2025-09-07 15:24:38 +0800

    Настройки автотестов с Pydantic. Практика работы с настройками.

--- tests/files/test_files.py
+++ tests/files/test_files.py
@@ -11,6 +11,7 @@ from clients.files.files_schema import (
     CreateFileResponseSchema,
     GetFileResponseSchema
 )
+from config import settings
 from fixtures.files import FileFixture
 from tools.allure.epics import AllureEpic
 from tools.allure.features import AllureFeature
@@ -42,7 +43,7 @@ class TestFiles:
     @allure.sub_suite(AllureStory.CREATE_ENTITY)
     @allure.title("Create file")
     def test_create_file(self, files_client: FilesClient):
-        request = CreateFileRequestSchema(upload_file="./testdata/files/image.png")
+        request = CreateFileRequestSchema(upload_file=settings.test_data.image_png_file)
         response = files_client.create_file_api(request)
         response_data = CreateFileResponseSchema.model_validate_json(response.text)
 
@@ -71,7 +72,7 @@ class TestFiles:
     @allure.sub_suite(AllureStory.VALIDATE_ENTITY)
     @allure.title("Create file with empty filename")
     def test_create_file_with_empty_filename(self, files_client: FilesClient):
-        request = CreateFileRequestSchema(filename="", upload_file="./testdata/files/image.png")
+        request = CreateFileRequestSchema(filename="", upload_file=settings.test_data.image_png_file)
         response = files_client.create_file_api(request)
         response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
 
@@ -86,7 +87,7 @@ class TestFiles:
     @allure.sub_suite(AllureStory.VALIDATE_ENTITY)
     @allure.title("Create file with empty directory")
     def test_create_file_with_empty_directory(self, files_client: FilesClient):
-        request = CreateFileRequestSchema(directory="", upload_file="./testdata/files/image.png")
+        request = CreateFileRequestSchema(directory="", upload_file=settings.test_data.image_png_file)
         response = files_client.create_file_api(request)
         response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
 
